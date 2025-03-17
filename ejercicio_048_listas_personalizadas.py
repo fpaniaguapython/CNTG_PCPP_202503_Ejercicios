08aad8cc11812b2ce4344003f1fa525e0b4ef74e
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-03-17 16:55:25 +0100

    Sesión 06 - Parcial

--- ejercicio_048_listas_personalizadas.py
+++ ejercicio_048_listas_personalizadas.py
@@ -1,21 +1,58 @@
 class Pelicula:
     def __init__(self, titulo):
         self.titulo = titulo
+    def __repr__(self):
+        return f'Título:{self.titulo}'
 
 class ListaPeliculas(list):
-    clase = Pelicula
+    __clase = Pelicula
     def __init__(self, *args, **kwargs):
+        print('__init__')
+        for object in args[0]:
+            if (not isinstance(object, ListaPeliculas.__clase)):
+                raise TypeError('No es una película')
         super().__init__(*args, **kwargs)
 
     def append(self, object):
-        if (not isinstance(object,Pelicula)):
+        print('append')
+        if (not isinstance(object, ListaPeliculas.__clase)):
             raise TypeError('No es una película')
         super().append(object)
 
+    def __setitem__(self, index, object):
+        print('__setitem__')
+        if (not isinstance(object, ListaPeliculas.__clase)):
+            raise TypeError('No es una película')
+        super().__setitem__(index, object)
 
-peliculas = ListaPeliculas()
+    def insert(self, index, object):
+        print('insert')
+        if (not isinstance(object, ListaPeliculas.__clase)):
+            raise TypeError('No es una película')
+        super().insert(index, object)
+
+
+
+# Construcción de una lista a partir de un iterable
+# peliculas = ListaPeliculas((1,2,3)) # Error
+peliculas = ListaPeliculas((Pelicula('Spiderman'), Pelicula('Superman')))
+
+# Construcción de una lista vacía
+# peliculas = ListaPeliculas()
+
+# Acceso con append
 peliculas.append(Pelicula('El Resplandor'))
 peliculas.append(Pelicula('Sharknado'))
-peliculas.append(Pelicula('El ataque del tiburón de 3 cabezas'))
-peliculas.append('Melón') # Error
+# peliculas.append('Melón') # Error
+
+# Acceso por índice
+# peliculas[0]='Melón' # Error
+peliculas[0]=Pelicula('El ataque del tiburón de 3 cabezas')
+
+# Acceso con insert
+# peliculas.insert(1,'Sandía') # Error
+peliculas.insert(1, Pelicula('Memorías de África'))
+
+
+
 print(peliculas)
---
+++ ejercicio_049_decorando_clases_completo.py
@@ -0,0 +1,55 @@
+def f_metodo_instancia(self):
+    print('Ejecutando método de instancia...:', self.numero)
+
+@classmethod
+def f_metodo_clase(cls):
+    print('Ejecutando método de clase...:', cls.atributo_de_clase)
+
+@staticmethod
+def f_metodo_estatico():
+    print('Ejecutando método estático...')
+
+def decorador(clase_a_decorar):
+    clase_a_decorar.metodo_instancia = f_metodo_instancia
+    clase_a_decorar.metodo_clase = f_metodo_clase
+    clase_a_decorar.metodo_estatico = f_metodo_estatico
+    clase_a_decorar.atributo_de_clase = 'Soy un atributo de clase'
+    original_init = clase_a_decorar.__init__
+    def inner_init(self, *args, **kwargs):
+        original_init(self, *args, **kwargs)
+        self.atributo_instancia = 'Soy un atributo de instancia'
+    clase_a_decorar.__init__ = inner_init
+
+    return clase_a_decorar
+
+@decorador
+class Factura:
+    def __init__(self, numero):
+        self.numero = numero
+
+    def get_numero(self):
+        return self.numero
+    
+factura_1 = Factura(100)
+factura_2 = Factura(102)
+
+factura_1.metodo_instancia() # Ejecutando método de instancia...: 100
+factura_1.metodo_clase() # Ejecuntando método de clase...: Soy un atributo de clase
+factura_1.metodo_estatico()# Ejecutando método estático...
+
+factura_2.metodo_instancia() # Ejecutando método de instancia...: 102
+factura_2.metodo_clase() # Ejecuntando método de clase...: Soy un atributo de clase
+factura_2.metodo_estatico() # Ejecutando método estático...
+
+print(factura_1.atributo_instancia) # Soy un atributo de instancia
+print(factura_2.atributo_instancia) # Soy un atributo de instancia
+factura_1.atributo_instancia = 'Soy un atributo de instancia modificado'
+print(factura_1.atributo_instancia) # Soy un atributo de instancia modificado
+print(factura_2.atributo_instancia) # Soy un atributo de instancia
+
+Factura.atributo_de_clase = 'Soy un atributo de clase modificado'
+factura_1.metodo_clase() # Ejecuntando método de clase...: Soy un atributo de clase modificado
+factura_2.metodo_clase() # Ejecuntando método de clase...: Soy un atributo de clase modificado
+
+print(Factura.__dict__) # metodo_instancia, metodo_clase, metodo_estatico, atributo_de_clase
+print(factura_1.__dict__) # numero, atributo_instancia
