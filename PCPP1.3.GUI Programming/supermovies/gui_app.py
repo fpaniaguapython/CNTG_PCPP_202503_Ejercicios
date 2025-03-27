d1f8ccd9967f749d46c0b0743a164f6cc79b8966
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-03-27 17:08:37 +0100

    Ejercicio GUI

---
+++ PCPP1.3.GUI Programming/supermovies/gui_app.py
@@ -0,0 +1,10 @@
+# 1. Menú (todavía no)
+# 2. (Frame 1) Barra de botones en un frame con un botón 'buscar' y un botón de 'guardar'.
+# 3. (Frame 2) Label (Título de la película) y una caja de texto.
+# 4. (Frame 2) Ficha de la película con Entrys para los siguientes campos:
+# - Título
+# - Director
+# - Sinopsis
+# - Año de estreno
+# 5. Cuando se pulse el botón 'buscar' se comprobar que hay algo en la caja de texto.
+# 6. Cuando se pulse el botón 'guardar' se comprueba que hay algo en la caja del título.
