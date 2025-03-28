abaaec6048918c1796544e1a643d7bd864ebf274
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-03-28 19:24:31 +0100

    Update ejercicio_021_canvas_image.py

--- PCPP1.3.GUI Programming/ejercicio_021_canvas_image.py
+++ PCPP1.3.GUI Programming/ejercicio_021_canvas_image.py
@@ -1,24 +1,26 @@
 import tkinter as tk
+import abc
 
-class Posicion:
-    def __init__(self, x, y):
+class F1:
+    def __init__(self, image_file, x, y, speed):
+        self.image = tk.PhotoImage(file=image_file)
         self.x = x
         self.y = y
+        self.speed = speed
 
-def mover(canvas : tk.Canvas, posicion : Posicion):
-    posicion.x+=1
-    canvas.create_image(posicion.x, 200, image=image)
+    def mover(self, canvas : tk.Canvas):
+        self.x+=self.speed
+        canvas.delete('all')
+        canvas.create_image(self.x, self.y, image=self.image)
 
 window = tk.Tk()
 canvas = tk.Canvas(window, width=400, height=400, bg='yellow')
-image = tk.PhotoImage(file='f1.png')
-button = tk.Button(window, text="Quit",command=window.destroy)
-posicion = Posicion(10, 200)
-button_mover = tk.Button(window, text="Mover",
-    command = lambda canvas_parametro=canvas, posicion_parametro=posicion : 
-        mover(canvas_parametro, posicion_parametro))
+
+f1 = F1(image_file='f1.png', x=10, y=200, speed=2)
+f1.mover(canvas=canvas)
+
+window.bind_all('z', lambda event, canvas_parametro=canvas, f1_parametro=f1 : 
+        f1_parametro.mover(canvas_parametro))
 
 canvas.grid(row=0)
-button.grid(row=1)
-button_mover.grid(row=2)
 window.mainloop()
