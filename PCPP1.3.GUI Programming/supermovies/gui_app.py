e09734b66b991bcc5929f2cb4846f37185b84c37
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-03-28 16:32:55 +0100

    Actualización de la aplicación

--- PCPP1.3.GUI Programming/supermovies/gui_app.py
+++ PCPP1.3.GUI Programming/supermovies/gui_app.py
@@ -10,6 +10,7 @@
 # 6. Cuando se pulse el botón 'guardar' se comprueba que hay algo en la caja del título.
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 
 class SuperMovieGUI:
     APP_TITLE = 'SuperMovie'
@@ -20,13 +21,22 @@ class SuperMovieGUI:
     WINDOW_MIN_HEIGHT = 480
     WINDOW_MAX_WIDTH = 1024
     WINDOW_MAX_HEIGHT = 768
-    ICON = 'icon.png'
-    def __init__(self):
-        self.__create_window()
-        self.__create_menu()
-        self.__create_buttons_bar()
-        self.__create_search_frame()
+    ICON = './icons/icon.png'
 
+    def __init__(self):
+        # Ventana principal
+        self.__create_window()
+        # Frames (no se muestran)
+        self.frames = dict() 
+        self.frames['create']=self.__create_search_frame()
+        self.frames['view']=self.__create_data_table_frame()
+        # Menú
+        self.__create_menu()
+        # Barra de botones
+        self.__create_buttons_bar()
+        # Mostrar el frame principal
+        self.__show_frame(self.frames['create'], fill_method='none', button_frame_enabled=True)
+        # Mainloop
         self.window.mainloop()
 
     def __create_window(self):
@@ -42,45 +52,60 @@ class SuperMovieGUI:
         # Tamaño
         self.window.geometry(f'{SuperMovieGUI.WINDOW_WIDTH}x{SuperMovieGUI.WINDOW_HEIGHT}')
         # Restricción de tamaño mínimo
-        #self.window.minsize(width=SuperMovieGUI.WINDOW_MIN_WIDTH, height=SuperMovieGUI.WINDOW_MIN_HEIGHT)
+        self.window.minsize(width=SuperMovieGUI.WINDOW_MIN_WIDTH, height=SuperMovieGUI.WINDOW_MIN_HEIGHT)
         # Restricción de tamaño máximo
         #self.window.maxsize(width=SuperMovieGUI.WINDOW_MAX_WIDTH, height=SuperMovieGUI.WINDOW_MAX_HEIGHT)
         # Restricción de cambio de tamaño
-        self.window.resizable(width=False, height=False)
+        #self.window.resizable(width=False, height=False)
 
         # Protocol
         # Intercepta el cierre de la ventana (no el fin de la aplicación)
         self.window.protocol('WM_DELETE_WINDOW', self.__do_final_tasks)
 
-
     def __create_buttons_bar(self):
         #region Creación del Frame
-        buttons_frame = tk.Frame(self.window, height=100, bg='#AAAAAA')
+        self.buttons_frame = tk.Frame(self.window, height=100, bg='#E0E0E0')
         # fill='x', hace que el Frame ocupe todo el ancho
         # anchor='n', hace que el Frame se 'ancle' al NORTE (parte superior del contenedor)
-        buttons_frame.pack(fill='x', anchor='n') 
+        self.buttons_frame.pack(fill='x', anchor='n') 
         #endregion
 
         #region Creación de los botones (dentro del frame)
-        self.b_search = tk.Button(buttons_frame, text='Buscar', height=2, command=self.__search_movie)
+        self.b_search = tk.Button(self.buttons_frame, text='Buscar', command=self.__search_movie)
+        self.search_image = tk.PhotoImage(file="./icons/search.png")
+        self.b_search.config(image=self.search_image)
         self.b_search.grid(row=0, column=0)
-        self.b_save = tk.Button(buttons_frame, text='Guardar', height=2, command=self.__save_movie)
+
+        self.b_save = tk.Button(self.buttons_frame, text='Guardar', command=self.__save_movie)
+        self.save_image = tk.PhotoImage(file="./icons/save.png")
+        self.b_save.config(image=self.save_image)
         self.b_save.grid(row=0, column=1)
+
+      
+        
+
+        self.all_buttons = []
+        self.all_buttons.append(self.b_search)
+        self.all_buttons.append(self.b_save)
+
         #endregion
 
     def __create_search_frame(self):
-        #region Creación del Frame
-        # Ocupa todo el espacio disponible, gracias a fill y a expand
+        X_LABELS = 50
+        X_ENTRIES = 150
+
+        #region Creación del LabelFrame
         self.search_frame = tk.LabelFrame(self.window, bg='#EEEEEE', text='Película', labelanchor=tk.NW)
-        self.search_frame.pack(fill='both', expand=True)
+        # Con esta configuración, el frame ocupa el espacio central
+        self.search_frame.config(width=600, height=450)
         # endregion
 
         #region Creación del Formulario de búsqueda
         self.l_search_title = tk.Label(self.search_frame, text='Título a buscar:')
-        self.l_search_title.place(x=200, y=50)
+        self.l_search_title.place(x=50, y=50)
 
         self.e_search_title = tk.Entry(self.search_frame, width=50)
-        self.e_search_title.place(x=300, y=50)
+        self.e_search_title.place(x=X_ENTRIES, y=50)
         
 
         self.l_title = tk.Label(self.search_frame, text='Título:')
@@ -88,22 +113,77 @@ class SuperMovieGUI:
         self.l_plot = tk.Label(self.search_frame, text='Sinopsis:')
         self.l_year = tk.Label(self.search_frame, text='Año de estreno:')
 
-        self.l_title.place(x=200, y=100)
-        self.l_director.place(x=200, y=140)
-        self.l_plot.place(x=200, y=180)
-        self.l_year.place(x=200, y=360)
+        self.l_title.place(x=X_LABELS, y=100)
+        self.l_director.place(x=X_LABELS, y=140)
+        self.l_plot.place(x=X_LABELS, y=180)
+        self.l_year.place(x=X_LABELS, y=360)
 
         self.e_title = tk.Entry(self.search_frame, width=50)
         self.e_director = tk.Entry(self.search_frame, width=50)
         self.t_plot = tk.Text(self.search_frame, height=10, width=40)
         self.sp_year = tk.Spinbox(self.search_frame, width=5, from_=1900, to=2025)
 
-        self.e_title.place(x=300, y=100)
-        self.e_director.place(x=300, y=140)
-        self.t_plot.place(x=300, y=180)
-        self.sp_year.place(x=300, y=360)
-
+        self.e_title.place(x=X_ENTRIES, y=100)
+        self.e_director.place(x=X_ENTRIES, y=140)
+        self.t_plot.place(x=X_ENTRIES, y=180)
+        self.sp_year.place(x=X_ENTRIES, y=360)
         #endregion
+
+        return self.search_frame
+
+    def __create_data_table_frame(self):
+        self.data_table_frame = tk.Frame(self.window)
+        
+        # Tabla
+        tree = ttk.Treeview(self.data_table_frame, columns=('Id', 'Title', 'Director', 'Plot', 'Year'), show='headings')
+        tree.column('Id', anchor='e', width=30)
+        tree.column('Title')
+        tree.column('Director', width=50)
+        tree.column('Plot')
+        tree.column('Year', anchor='e', width=30)
+
+        # Estilos
+        style = ttk.Style()
+        style.theme_use('default') # default, clam, alt, classic, vista
+        style.configure('Treeview',
+                        rowheight=30)
+        style.configure('Treeview.Heading', 
+                        foreground='white',
+                        background='#909090',
+                        padding=(5, 10))
+
+        # Encabezados
+        tree.heading('Id', text='Id')
+        tree.heading('Title', text='Título')
+        tree.heading('Director', text='Director')
+        tree.heading('Plot', text='Sinopsis')
+        tree.heading('Year', text='Estreno')
+        
+        # Datos
+        peliculas = [
+            (1, 'Tiburón', 'Steven Spielberg', 
+            'En la isla turística de Amity, un gigantesco tiburón blanco comienza a atacar a los bañistas. '
+            'Un jefe de policía, un biólogo marino y un cazador de tiburones se embarcan en una peligrosa misión para detenerlo.',
+            1975),
+            
+            (2, 'Alien, el octavo pasajero', 'Ridley Scott',  
+            'La tripulación de la nave Nostromo recibe una señal de auxilio desde un planeta desconocido. '
+            'Al investigar, descubren una forma de vida alienígena letal que comienza a acecharlos dentro de la nave.',
+            1979),
+            
+            (3, 'Alguien voló sobre el nido del cuco', 'Miloš Forman',
+            'Randle McMurphy, un hombre rebelde, es internado en un hospital psiquiátrico, donde se enfrenta a la estricta enfermera Ratched. '
+            'Su espíritu de libertad inspira a los otros pacientes, generando un choque de poder dentro de la institución.',
+            1975)
+        ]
+
+        for fila in peliculas:
+            tree.insert('', 'end', values=fila)
+
+        # Posicionamiento de la tabla
+        tree.pack(expand=True, fill='both')
+
+        return self.data_table_frame
 
     def __create_menu(self):
         # *** MENÚ PRINCIPAL ***
@@ -126,7 +206,7 @@ class SuperMovieGUI:
         self.file_menu.add_separator() # Dibujar una línea horizontal
         self.file_menu.add_command(label='Exit', underline=0, command=self.__exit_app, accelerator='Ctrl-E')
 
-        self.window.bind_all("<Control-e>", self.__exit_app)
+        self.window.bind_all('<Control-e>', self.__exit_app)
 
 
         # *** MENU MOVIES ***
@@ -134,8 +214,10 @@ class SuperMovieGUI:
         self.main_menu.add_cascade(label='Movies', menu=self.movies_menu, underline=0) 
         
         # *** MENÚ FILE - OPCIONES
-        self.movies_menu.add_command(label='Add', underline=0, command=self.__show_not_implemented_error)
-        self.movies_menu.add_command(label='View', underline=1, command=self.__show_not_implemented_error)
+        self.movies_menu.add_command(label='Add', underline=0, 
+            command= lambda target = self.frames['create'] : self.__show_frame(target, fill_method='none', button_frame_enabled=True))
+        self.movies_menu.add_command(label='View', underline=1, 
+            command= lambda target = self.frames['view'] : self.__show_frame(target, fill_method='both', button_frame_enabled=False))
 
         # *** MENU HELP ***
         self.help_menu = tk.Menu(self.main_menu, tearoff=0)
@@ -185,3 +267,25 @@ class SuperMovieGUI:
     def __do_final_tasks(self):
         print('Haciendo las tareas de cierre de la aplicación...')
         self.window.destroy()
+
+    def __show_frame(self, current_frame, fill_method, button_frame_enabled):
+        # Ocultar la barra de herramientas o desactivar los botones
+        if button_frame_enabled:
+            # Opción 1. Mostrar
+            # self.buttons_frame.pack(fill='x', anchor='n') 
+
+            # Opción 2. Activar
+            list(map(lambda boton : boton.config(state='active'), self.all_buttons))
+        else:
+            # Opción 1. Ocultar
+            # self.buttons_frame.forget()
+
+            # Opción 2. Desactivar
+            list(map(lambda boton : boton.config(state='disabled'), self.all_buttons))
+        
+        # Ocultar todos los frames
+        for frame in self.frames.values():
+            frame.pack_forget()
+
+        # Mostrar el frame activo        
+        current_frame.pack(fill=fill_method, expand=True)        
