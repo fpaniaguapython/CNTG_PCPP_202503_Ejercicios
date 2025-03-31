544c3e76ed6ee212cd09a68d9977774ad3331953
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-03-31 18:07:22 +0200

    Ejercicio CHAT

---
+++ PCPP1.4 Working-with-RESTful-APIs/ejercicio_03_chat_client_socket.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST_IP = '127.0.0.1'
+HOST_PORT = 8021
+
+# Creamos el scoket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Indicamos IP y port
+sock.connect((HOST_IP, HOST_PORT))
+# Envío de mensaje
+mensaje='Hola, soy el cliente'
+sock.send(mensaje.encode('utf-8'))
+print(f'Enviado al servidor el mensaje "{mensaje}"')
+# Lectura de mensaje
+respuesta = sock.recv(1000)
+print(f'Recibiendo el mensaje del servidor "{respuesta.decode('utf-8')}"')
+# Cierre
+sock.shutdown(socket.SHUT_RDWR)
+sock.close()
