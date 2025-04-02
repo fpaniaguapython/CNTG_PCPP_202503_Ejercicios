484ae30d1fdc953a2cccfd309a98027ee28cea3d
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-04-02 13:00:27 +0200

    Sesión 17

--- PCPP1.4 Working-with-RESTful-APIs/ejercicio_16_ejercicio_xml.py
+++ PCPP1.4 Working-with-RESTful-APIs/ejercicio_16_ejercicio_xml.py
@@ -3,3 +3,19 @@
 # Director
 # Genero
 # Guardar a fichero para comprobar el resultado
+import xml.etree.ElementTree
+
+# Leyendo el documento y haciendo un parse 
+tree = xml.etree.ElementTree.parse('ejercicio_16.xml')
+# Obtener el elemento raíz
+peliculas = tree.getroot()
+# Construimos el nuevo elemento
+nueva_pelicula = xml.etree.ElementTree.Element('pelicula')
+# Asignamos subelementos al elemento nuevo
+xml.etree.ElementTree.SubElement(nueva_pelicula, 'titulo').text = 'Tiburón'
+xml.etree.ElementTree.SubElement(nueva_pelicula, 'director').text = 'Steven Spielberg'
+xml.etree.ElementTree.SubElement(nueva_pelicula, 'genero').text = 'Terror'
+# Vinculamos el elemento nuevo al elemento raíz
+peliculas.append(nueva_pelicula)
+xml.etree.ElementTree.indent(tree, space="  ", level=0)
+tree.write('videoteca.xml', method='')
